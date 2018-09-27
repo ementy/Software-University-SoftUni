7d9a15290d6eb8ff21c15d643dc50bb4da68f9e4
ementy <mariyan.todorov13@gmailcom>
2018-09-27 22:03:27 +0300

    add http homework

---
+++ C# Web/C# Web Development Basics/04.HttpProtocol/P01_UrlDecode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+
+namespace P01_UrlDecode
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(WebUtility.UrlDecode(input));
+        }
+    }
+}
